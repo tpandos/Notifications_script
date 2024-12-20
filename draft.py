ed9aaa4b12a1219e929780d77e2405160ff97028
tpandos <t.pandos@protonmail.com>
2024-12-20 17:13:11 -0500

    removed colus C, E, F, M, not needed or blank

--- draft.py
+++ draft.py
@@ -27,17 +27,17 @@ data_clean_headers = excel_data.iloc[14:].reset_index(drop=True)
 data_clean_headers.columns = data_clean_headers.iloc[0]
 
 #reset index for final excel data 
-final_data = data_clean_headers.drop(0).reset_index(drop=True)
+data_clean_headers = data_clean_headers.drop(0).reset_index(drop=True)
 
 #remove blank cols (C, F), remove Business Unit (E), Training Date (M)
 #0-based index, columns 2, 4, 5, 12 
 
+cols_to_remove = [2, 4, 5, 12]
+#df = df.drop(specify cols to drop df.columns[cols_to_remove], axis=1)
+final_data = data_clean_headers.drop(data_clean_headers.columns[cols_to_remove], axis=1)
+
 #save the modified file
 final_data.to_excel(input_file, index=False)
-
-#
-
-
 
 print("This is the end")
 input("Press Enter to exit")
